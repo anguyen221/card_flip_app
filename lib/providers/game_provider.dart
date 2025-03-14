a182606df2b2740f16a01a70efd441f5ebe2477f
anguyen221 <anguyen221@student.gsu.edu>
2025-03-14 08:34:48 -0400

    Implemented card  matching logic

--- lib/providers/game_provider.dart
+++ lib/providers/game_provider.dart
@@ -2,37 +2,71 @@ import 'package:flutter/material.dart';
 import '../models/card_model.dart';
 
 class GameProvider extends ChangeNotifier {
-  List<CardModel> _cards = [];
-
-  List<CardModel> get cards => _cards;
+  List<CardModel> cards = [];
+  int? firstFlippedIndex;
+  bool isChecking = false;
 
   GameProvider() {
-    _initializeGame();
+    _initializeCards();
   }
 
-  void _initializeGame() {
+  void _initializeCards() {
     List<String> images = [
-      'assets/card1.png', 'assets/card1.png',
-      'assets/card2.png', 'assets/card2.png',
-      'assets/card3.png', 'assets/card3.png',
-      'assets/card4.png', 'assets/card4.png',
-      'assets/card5.png', 'assets/card5.png',
-      'assets/card6.png', 'assets/card6.png',
-      'assets/card7.png', 'assets/card7.png',
-      'assets/card8.png', 'assets/card8.png',
+      'assets/card1.png',
+      'assets/card2.png',
+      'assets/card3.png',
+      'assets/card4.png',
+      'assets/card5.png',
+      'assets/card6.png',
+      'assets/card7.png',
+      'assets/card8.png',
     ];
+    
+    cards = List.generate(8, (index) => CardModel(id: index, image: images[index]))
+      ..addAll(List.generate(8, (index) => CardModel(id: index + 8, image: images[index])));
 
-    images.shuffle();
-
-    _cards = List.generate(16, (index) => 
-      CardModel(id: index, image: images[index])
-    );
-
+    cards.shuffle();
     notifyListeners();
   }
 
   void flipCard(int index) {
-    _cards[index].isFaceUp = !_cards[index].isFaceUp;
+    if (isChecking || cards[index].isFaceUp || cards[index].isMatched) return;
+
+    cards[index].isFaceUp = true;
     notifyListeners();
+
+    if (firstFlippedIndex == null) {
+      firstFlippedIndex = index;
+    } else {
+      _checkMatch(index);
+    }
+  }
+
+  void _checkMatch(int secondIndex) {
+    int firstIndex = firstFlippedIndex!;
+    isChecking = true;
+
+    if (cards[firstIndex].image == cards[secondIndex].image) {
+      cards[firstIndex].isMatched = true;
+      cards[secondIndex].isMatched = true;
+      isChecking = false;
+      if (checkWin()) {
+        debugPrint("Test win");
+      }
+    } else {
+      Future.delayed(const Duration(milliseconds: 1000), () {
+        cards[firstIndex].isFaceUp = false;
+        cards[secondIndex].isFaceUp = false;
+        isChecking = false;
+        notifyListeners();
+      });
+    }
+
+    firstFlippedIndex = null;
+    notifyListeners();
+  }
+
+  bool checkWin() {
+    return cards.every((card) => card.isMatched);
   }
 }
